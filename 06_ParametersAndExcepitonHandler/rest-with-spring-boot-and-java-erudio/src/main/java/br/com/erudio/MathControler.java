951ff6644f77d4e2de35e330d4e3e2f7aeae9e77
Danilo Rodrigues de Oliveira <danilo.rodrigues38@gmail.com>
2023-01-02 18:28:45 -0300

    Second class with REST, Spring Boot 3 and Java

---
+++ 06_ParametersAndExcepitonHandler/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/MathControler.java
@@ -0,0 +1,47 @@
+package br.com.erudio;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MathControler {
+
+	private final AtomicLong counter = new AtomicLong();
+	
+	@RequestMapping(value = "/sum/{numberOne}/{numberTwo}",
+			method=RequestMethod.GET)
+	public Double sum(
+			@PathVariable(value = "numberOne") String numberOne,
+			@PathVariable(value = "numberTwo") String numberTwo
+		) throws Exception{
+		
+		if(!isNumeric(numberOne) || !isNumeric(numberTwo)) {
+			throw new Exception();
+		}
+		return convertToDouble(numberOne) + convertToDouble(numberTwo);
+	}
+
+	private Double convertToDouble(String strNumber) {
+		if (strNumber == null) return 0D;
+		/* No Brasil a casa decinal é separada por virgula "," e como toda a linguagem de programação
+		 * é baseada nos EUA a separação decimal é feita pelo ponto "." então todas as  linguagem  de
+		 * programação somente aceitam o ponto "." como separador decimal, nosso código vai tratar  o
+		 * separador, aceitando tanto a virgula quanto o ponto.
+		 */
+		String number = strNumber.replaceAll(",", ".");
+		if (isNumeric(number)) return Double.parseDouble(number);
+		return 0D;
+	}
+
+	private boolean isNumeric(String strNumber) {
+		if (strNumber == null) return false;
+		String number = strNumber.replaceAll(",", ".");
+		return number.matches("[-+]?[0-9]*\\.?[0-9]+");
+	}
+	
+	
+}
